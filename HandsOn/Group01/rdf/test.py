f76c850fcd03ee3355e29d267a4213c69b6ff173
Rubén Cid <rubencid001@gmail.com>
2021-12-08 19:02:14 +0100

    Todas las Correcciones y Endpoint

--- HandsOn/Group01/rdf/test.py
+++ HandsOn/Group01/rdf/test.py
@@ -5,7 +5,7 @@ from rdflib.plugins.sparql import prepareQuery
 
 
 G = Graph()
-G.parse(r"./Group01/rdf/linked_museos.ttl", format="ttl")
+G.parse(r"museos_linked.ttl", format="ttl")
 print("TEXT PARSED")
 # Carga de los Namespaces
 
@@ -13,12 +13,11 @@ prop = Namespace("https://www.mapmadrid.org/ontologia/Propiedades/")
 clases = Namespace("https://www.mapmadrid.org/ontologia/Clases/")
 
 #
-
-q = prepareQuery("""SELECT DISTINCT ?value
+q = prepareQuery("""
+    SELECT DISTINCT ?prop, COUNT(DISTINCT ?values) as ?posiblesValores
     WHERE{
-        ?name rdf:type <https://www.mapmadrid.org/ontology/0.1.0/Clases/Museos>.
-        ?name <https://www.mapmadrid.org/ontology/0.1.0/Propiedades/isLocated> ?loc.
-        ?loc <https://www.mapmadrid.org/ontology/0.1.0/Propiedades/inNeighborhood> ?value.
+        ?name rdf:type <https://www.mapmadrid.org/ontologia/Museo>.
+        ?name ?prop ?value.
     }
     """
 )
